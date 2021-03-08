b8e3f878c057f51ba7550c8289a0568b6eff7496
ahmed-elsaid <ahm.abdrahman@gmail.com>
2021-03-08 18:01:36 +0200

    add auth variables to environment auth service

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'ahm90.us', // the auth0 domain prefix
+    audience: 'coffee-shop', // the audience set for the auth0 app
+    clientId: 'U9zDhDjqIgmlwObp3a81vNCBPDGE0xhX', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
